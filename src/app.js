b486aff415c82fbcb93bf3461bcb8f1885fd5e23
Synox <synox@users.noreply.github.com>
2016-09-06 08:53:28 +0200

    use sticky headers

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
 // Vendor-Imports
-import angular from 'angular';
-import uiRouter from 'angular-ui-router';
-import uiBootstrap from 'angular-ui-bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'babel-polyfill';
+import angular from "angular";
+import uiRouter from "angular-ui-router";
+import uiBootstrap from "angular-ui-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "babel-polyfill";
+import angularStickyfill from "angular-stickyfill";
+import "angular-stickyfill/dist/angular-stickyfill.css";
+import Mailbox from "./mailbox/mailbox";
 
 // Interne Modul-Imports
-import Mailbox from './mailbox/mailbox';
 
 angular.module('app', [
-    uiRouter, uiBootstrap, Mailbox.name
+    uiRouter, uiBootstrap, Mailbox.name, angularStickyfill
 ])
 
     .constant('config', {
-        'backend_url': './backend.php',
+        'backend_url': 'http://dubgo.com/m2/backend.php',
         'reload_interval_ms': 10000
     })
