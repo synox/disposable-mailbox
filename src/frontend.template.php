7097c008e2530dce0d6ce40b729b54bc6c469f3e
Synox <synox@users.noreply.github.com>
2018-01-18 21:52:47 +0100

    cleanup

--- src/frontend.template.php
+++ src/frontend.template.php
@@ -215,35 +215,30 @@ $purifier = new HTMLPurifier($purifier_config);
                             ?>
 
                             <div class="mt-2 card-text">
-                                <div>
-                                    <?php
-                                    $safeHtml = $purifier->purify($email->textHtml);
+                                <?php
+                                $safeHtml = $purifier->purify($email->textHtml);
 
-                                    $safeText = $purifier->purify($email->textPlain);
-                                    $safeText = nl2br($safeText);
-                                    $safeText = \AutoLinkExtension::auto_link_text($safeText);
+                                $safeText = $purifier->purify($email->textPlain);
+                                $safeText = nl2br($safeText);
+                                $safeText = \AutoLinkExtension::auto_link_text($safeText);
 
-                                    $hasHtml = strlen(trim($safeHtml)) > 0;
-                                    $hasText = strlen(trim($safeText)) > 0;
+                                $hasHtml = strlen(trim($safeHtml)) > 0;
+                                $hasText = strlen(trim($safeText)) > 0;
 
-                                    if ($config['prefer_plaintext']) {
-                                        if ($hasText) {
-                                            echo $safeText;
-                                        } else {
-                                            echo $safeHtml;
-                                        }
+                                if ($config['prefer_plaintext']) {
+                                    if ($hasText) {
+                                        echo $safeText;
                                     } else {
-                                        if ($hasHtml) {
-                                            echo $safeHtml;
-                                        } else {
-                                            echo $safeText;
-                                        }
+                                        echo $safeHtml;
                                     }
-                                    ?>
-                                </div>
-                                <div>
-                                    <?php ?>
-                                </div>
+                                } else {
+                                    if ($hasHtml) {
+                                        echo $safeHtml;
+                                    } else {
+                                        echo $safeText;
+                                    }
+                                }
+                                ?>
                             </div>
 
                         </div>
